6f712caab506d467c60e8ce841d9294d67280181
AICI-Global-84 <aiciglobal01@gmail.com>
2024-10-05 01:46:47 +0700

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from .CombineAudioVideoAndUpload import CombineAudioVideoAndUpload 
+
+NODE_CLASS_MAPPINGS = {
+    "CombineAudioVideoAndUpload": CombineAudioVideoAndUpload
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "CombineAudioVideoAndUpload": "Combine Audio and Video, Upload to Drive"
+}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
