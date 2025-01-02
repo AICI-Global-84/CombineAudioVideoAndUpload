f42ef39275e1fabdb904bce4aac22eaa9d49b7dc
AICI-Global-84 <aiciglobal01@gmail.com>
2025-01-02 17:13:45 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,17 @@
-from .CombineAudioVideoAndUpload import CombineAudioVideoAndUpload, VideoAudioLoader, LoadAudioURL
+from .CombineAudioVideoAndUpload import CombineAudioVideoAndUpload, VideoAudioLoader, LoadAudioURL, CombineAudio
 
 NODE_CLASS_MAPPINGS = {
     "CombineAudioVideoAndUpload": CombineAudioVideoAndUpload,
     "VideoAudioLoader": VideoAudioLoader,
-    "LoadAudioURL": LoadAudioURL
+    "LoadAudioURL": LoadAudioURL,
+    "CombineAudio": CombineAudio
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "CombineAudioVideoAndUpload": "Combine Audio and Video, Upload to Drive",
     "VideoAudioLoader": "Load Video/Audio from URL or Upload",
-    "LoadAudioURL": "Load Audio from URL"
+    "LoadAudioURL": "Load Audio from URL",
+    "CombineAudio": "Combine Two Audios (Music offsets w.r.t Voice)"
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
